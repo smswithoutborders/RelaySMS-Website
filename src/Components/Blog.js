2048f4b388b32bd060abd0013ba88d4e2eee6343
Promise Fru <promisefru1@gmail.com>
2024-07-23 10:26:29 +0100

    Revert "change made on the dev branch and merged into the staging branch"

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -5,7 +5,6 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme, Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { Container, Row } from "react-bootstrap";
-import "../App.css";
 
 const MultiItemCarousel = () => {
 	const theme = useTheme();
@@ -86,7 +85,7 @@ const MultiItemCarousel = () => {
 			style={{
 				padding: "0 20px",
 				marginBottom: "20px",
-				maxWidth: "1500px",
+				maxWidth: "2000px",
 				margin: "0 auto"
 			}}
 			sx={{
