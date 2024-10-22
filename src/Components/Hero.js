611da8577c88ac9cc05e1fe21a3e09f6c1c573e7
mildrette <mildredfonka1@gmail.com>
2024-10-22 11:37:45 +0100

    added download section to the relaysms website and changed the downloads buttons

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -8,7 +8,9 @@ import How from "../Components/how";
 import Blog from "../Components/Blog";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom";
+
+import "../App.css";
 
 const HeroSection = () => {
 	const { t, i18n } = useTranslation();
@@ -23,44 +25,38 @@ const HeroSection = () => {
 	}, []);
 
 	return (
-		<div className="hero-container" dir={isFarsi ? "rtl" : "ltr"}>
-			<Container fluid="xl">
-				<Row className="align-items-center">
-					<Col lg={6} className="position-relative z-index-10 pb-5 pt-5" data-aos="fade-right">
-						<main className="text-center text-lg-start hero-main">
-							<h1 className="hero-title hero-highlight">
-								{t("Landing.h1")} <br />
-							</h1>
-							<p className="hero-description">{t("Landing.h2")}</p>
-							<div className="hero-buttons d-flex flex-column flex-md-row justify-content-md-start justify-content-center">
-								<Button
-									as={Link} // Use Link for routing
-									to="/Download"
-									className="hero-btn Download-btn mx-auto mx-md-0"
-									data-aos="fade-up"
-								>
-									<span className="button-title">{t("Landing.Android")}</span>
-									<FontAwesomeIcon icon={faArrowDown} className="hero-icon apple" />
-								</Button>
+		<>
+			<section className="hero-container">
+				<Container className={`hero-main ${isFarsi ? "rtl" : ""}`}>
+					<Row className="align-items-center">
+						<Col lg={6} md={10} className="text-start">
+							<h1 className="hero-title">{t("Landing.h1")}</h1>
+							<div className="hero-description-section mt-4">
+								<h5 className="hero-description">{t("Landing.h2")}</h5>
 							</div>
-						</main>
-					</Col>
-					<Col lg={6} className="image-section d-lg-block position-relative" data-aos="fade-left">
-						<Image
-							src="/Relaysms.png"
-							alt="RelaySMS application interface"
-							className="hero-image"
-						/>
-					</Col>
-				</Row>
-			</Container>
+							<Button as={Link} to="/Download" data-aos="fade-up" className="btn-download btn-lg">
+								{t("Landing.Android")}
+								<FontAwesomeIcon icon={faArrowDown} className="arrow-icon" />
+							</Button>
+						</Col>
+						<Col lg={6} md={10} className="image-section" data-aos="fade-left">
+							<Image
+								src="/Relaysms.png"
+								alt="RelaySMS application interface"
+								className="hero-image"
+							/>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+
 			<div data-aos="fade-up">
 				<How />
 			</div>
 			<div data-aos="fade-up" data-aos-delay="200">
 				<Blog />
 			</div>
-		</div>
+		</>
 	);
 };
 
