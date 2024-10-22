7950e843f21f5b71eb231e05c777c665c4e74d96
mildrette <mildredfonka1@gmail.com>
2024-10-22 13:37:58 +0100

    update to the landing page

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,16 +1,14 @@
 import React, { useEffect } from "react";
 import { Button, Container, Row, Col, Image } from "react-bootstrap";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useTranslation } from "react-i18next";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import How from "../Components/how";
 import Blog from "../Components/Blog";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
-
-import "../App.css";
 
 const HeroSection = () => {
 	const { t, i18n } = useTranslation();
@@ -25,38 +23,34 @@ const HeroSection = () => {
 	}, []);
 
 	return (
-		<>
-			<section className="hero-container">
-				<Container className={`hero-main ${isFarsi ? "rtl" : ""}`}>
-					<Row className="align-items-center">
-						<Col lg={6} md={10} className="text-start">
-							<h1 className="hero-title">{t("Landing.h1")}</h1>
-							<div className="hero-description-section mt-4">
-								<h5 className="hero-description">{t("Landing.h2")}</h5>
-							</div>
+		<div className="hero-container" dir={isFarsi ? "rtl" : "ltr"}>
+			<Container fluid="xl">
+				<Row className="align-items-center">
+					<Col lg={6} className="position-relative z-index-10 pb-5 pt-5" data-aos="fade-right">
+						<main className="text-center text-lg-start hero-main">
+							<h1 className="hero-title hero-highlight">
+								{t("Landing.h1")} <br />
+							</h1>
+
+							<p className="hero-description">{t("Landing.h2")}</p>
 							<Button as={Link} to="/Download" data-aos="fade-up" className="btn-download btn-lg">
 								{t("Landing.Android")}
 								<FontAwesomeIcon icon={faArrowDown} className="arrow-icon" />
 							</Button>
-						</Col>
-						<Col lg={6} md={10} className="image-section" data-aos="fade-left">
-							<Image
-								src="/Relaysms.png"
-								alt="RelaySMS application interface"
-								className="hero-image"
-							/>
-						</Col>
-					</Row>
-				</Container>
-			</section>
-
+						</main>
+					</Col>
+					<Col lg={6} className="d-lg-block position-relative" data-aos="fade-left">
+						<Image src="/Relaysms.png" alt="relay" className="hero-image" />
+					</Col>
+				</Row>
+			</Container>
 			<div data-aos="fade-up">
 				<How />
 			</div>
 			<div data-aos="fade-up" data-aos-delay="200">
 				<Blog />
 			</div>
-		</>
+		</div>
 	);
 };
 
