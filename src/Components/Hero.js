c4f79093aaaf20be10e05707b0c8cb8b0db96ce2
mildrette <mildredfonka1@gmail.com>
2024-10-23 16:10:06 +0100

    Refactor navbar for better alignment and spacing; improve hero section layout and responsiveness across devices

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,16 +1,15 @@
 import React, { useEffect } from "react";
-import { Button, Container, Row, Col, Image } from "react-bootstrap";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Row, Col, Navbar, Nav, Button } from "react-bootstrap";
+import { FaDownload, FaGithub } from "react-icons/fa";
+import LanguageSwitcher from "./LanguageSwitcher";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useTranslation } from "react-i18next";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import How from "../Components/how";
-import Blog from "../Components/Blog";
+import "../App.css";
 
-const HeroSection = () => {
+const Hero = () => {
 	const { t, i18n } = useTranslation();
 	const isFarsi = i18n.language === "fa";
 
@@ -23,35 +22,68 @@ const HeroSection = () => {
 	}, []);
 
 	return (
-		<div className="hero-container" dir={isFarsi ? "rtl" : "ltr"}>
-			<Container fluid="xl">
-				<Row className="align-items-center">
-					<Col lg={6} className="position-relative z-index-10 pb-5 pt-5" data-aos="fade-right">
-						<main className="text-center text-lg-start hero-main">
-							<h1 className="hero-title hero-highlight">
-								{t("Landing.h1")} <br />
-							</h1>
+		<>
+			{/* Responsive Navbar */}
+			<Navbar
+				expand="lg"
+				bg="light"
+				fixed="top"
+				dir={isFarsi ? "rtl" : "ltr"}
+				className="custom-navbar"
+			>
+				{/* Logo aligned to the left */}
+				<Navbar.Brand href="#home" className="navbar-logo">
+					<img src="./logo.png" alt="Logo" height="40" className="logo" />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="navbar-nav" />
+				<Navbar.Collapse id="navbar-nav" className="justify-content-end">
+					{/* Links aligned to the right */}
+					<Nav className="nav-links">
+						<Nav.Link href="/" className="mx-2">
+							{t("Nav.Home")}
+						</Nav.Link>
+						<Nav.Link href="https://blog.smswithoutborders.com/" className="mx-2">
+							{t("Nav.Blog")}
+						</Nav.Link>
+						<Nav.Link href="https://docs.smswithoutborders.com/" className="mx-2">
+							{t("Nav.Help")}
+						</Nav.Link>
+						<Nav.Link as={Link} to="/Contact_Us" className="mx-2">
+							{t("Nav.Contact")}
+						</Nav.Link>
+					</Nav>
+					{/* Social Media Icons */}
+					<Nav className="social-icons">
+						<Nav.Link href="https://x.com/RelaySMS" className="mx-2">
+							<img src="./x.svg" alt="X logo" height="20" />
+						</Nav.Link>
+						<Nav.Link href="https://github.com/smswithoutborders" className="mx-2">
+							<FaGithub />
+						</Nav.Link>
+					</Nav>
+					<LanguageSwitcher className="mx-2" />
+				</Navbar.Collapse>
+			</Navbar>
 
-							<p className="hero-description">{t("Landing.h2")}</p>
-							<Button as={Link} to="/Download" data-aos="fade-up" className="btn-download">
-								{t("Landing.Android")}
-								<FontAwesomeIcon icon={faArrowDown} className="arrow-icon" />
+			{/* ========== Hero Section =============*/}
+			<div className="hero-section" style={{ marginTop: "50px" }} dir={isFarsi ? "rtl" : "ltr"}>
+				<Container fluid>
+					<Row className="align-items-center">
+						<Col md={6} xs={12} className="text-md-left" data-aos="fade-up">
+							<h1 className="display-4">{t("Landing.h1")}</h1>
+							<p className="Description">{t("Landing.h2")}</p>
+							<Button variant="primary" size="lg" className="download-button">
+								{t("Landing.Android")} <FaDownload />
 							</Button>
-						</main>
-					</Col>
-					<Col lg={6} className="d-lg-block position-relative" data-aos="fade-left">
-						<Image src="/Relaysms.png" alt="relay" className="hero-image" />
-					</Col>
-				</Row>
-			</Container>
-			<div data-aos="fade-up">
-				<How />
+						</Col>
+						<Col md={6} xs={12} className="text-center" data-aos="fade-up">
+							<img src="/Relaysms.png" alt="relay" className="hero-image img-fluid" />
+						</Col>
+					</Row>
+				</Container>
 			</div>
-			<div data-aos="fade-up" data-aos-delay="200">
-				<Blog />
-			</div>
-		</div>
+		</>
 	);
 };
 
-export default HeroSection;
+export default Hero;
